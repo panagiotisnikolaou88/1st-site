631c2abce672a30496071ffe9560f8e3ca5a4ec1
panagiotisnikolaou88 <pan.nik636@gmail.com>
2021-08-09 13:02:59 +0100

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu_button = document.getElementById('menu_button');
+const side_menu = document.getElementById('side_menu');
+
+menu_button.addEventListener('click', () => {
+
+    side_menu.classList.toggle('show');
+} )  
+
+
