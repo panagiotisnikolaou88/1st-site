97444956a30621ce77e76709965178343562128b
panagiotisnikolaou88 <pan.nik636@gmail.com>
2021-08-10 14:17:44 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const menu_button = document.getElementById('menu_button');
 const side_menu = document.getElementById('side_menu');
 
-menu_button.addEventListener('click', () => {
-
-    side_menu.classList.toggle('show');
-} )  
 
 
+function openNav(){
+
+    document.getElementById('side_menu').style.width="50%";
+    document.getElementById('menu_button').style.display="none";
+
+
+}
+
+function closeNav(){
+
+document.getElementById('side_menu').style.width="0"
+document.getElementById('menu_button').style.display="block";
+
+}
